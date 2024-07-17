ed08993e4f6e369545a4ceb307ff8ba09bb1aa81
asmashaikh3 <ashaikh3@academic.rrc.ca>
2024-07-17 03:07:06 -0500

    Define ransackable_attributes in AdminUser model

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -1,6 +1,10 @@
 class AdminUser < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, 
-         :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :recoverable, :rememberable, :validatable
+
+  # Add the ransackable_attributes class method
+  def self.ransackable_attributes(auth_object = nil)
+    ["created_at", "email", "id", "remember_created_at", "reset_password_sent_at", "updated_at"]
+  end
 end
