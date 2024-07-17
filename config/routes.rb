ac0f6f3d62f7f861c89a265f0cece432d64910b2
asmashaikh3 <ashaikh3@academic.rrc.ca>
2024-07-17 15:02:03 -0500

    Update routes to include products and set products#index as root

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  get 'products/index'
-  get 'products/show'
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
 
-  root to: 'home#index'
+  root to: 'products#index'
+  resources :products, only: [:index, :show]
 end
