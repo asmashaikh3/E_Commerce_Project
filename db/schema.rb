557814d8b39e07c397c9922b38af3965d0a5edd1
asmashaikh3 <ashaikh3@academic.rrc.ca>
2024-07-17 09:33:17 -0500

    Generate Product model and migrate database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_17_071120) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_17_143236) do
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
     t.text "body"
@@ -37,6 +37,17 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_17_071120) do
     t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
   end
 
+  create_table "products", force: :cascade do |t|
+    t.string "product_name"
+    t.text "description"
+    t.decimal "price"
+    t.integer "stock_quantity"
+    t.integer "category_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_products_on_category_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -49,4 +60,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_17_071120) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "products", "categories"
 end
