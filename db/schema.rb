301f04003e5fe5439f19758b997ef36117af7665
asmashaikh3 <ashaikh3@academic.rrc.ca>
2024-07-18 16:23:13 -0500

    Seed initial content for contact and about pagesgit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_17_192854) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_18_212129) do
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
     t.text "body"
@@ -89,6 +89,13 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_17_192854) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_orders_on_user_id"
+  end
+
+  create_table "pages", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "products", force: :cascade do |t|
