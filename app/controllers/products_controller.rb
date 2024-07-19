be383a076299b90ce70a65b3823d3d3224f132dc
asmashaikh3 <ashaikh3@academic.rrc.ca>
2024-07-19 18:23:54 -0500

    search functionality successfully implemented

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,16 @@
 class ProductsController < ApplicationController
   def index
-    @products = Product.all
+    @categories = Category.all
+
+    if params[:search].present? && params[:category_id].present?
+      @products = Product.where("product_name LIKE ? OR description LIKE ?", "%#{params[:search]}%", "%#{params[:search]}%").where(category_id: params[:category_id])
+    elsif params[:search].present?
+      @products = Product.where("product_name LIKE ? OR description LIKE ?", "%#{params[:search]}%", "%#{params[:search]}%")
+    elsif params[:category_id].present?
+      @products = Product.where(category_id: params[:category_id])
+    else
+      @products = Product.all
+    end
   end
 
   def show
